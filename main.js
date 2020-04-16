1e6e3be46a247788ca7484de7d4a63097e64a8c5
eaajome <48531953+eaajome@users.noreply.github.com>
2020-04-16 18:41:53 +0200

    Survey v1

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 // import your components, pages and services
 import NavBar from "./components/navbar.js";
 import HomePage from "./pages/home.js";
+import SurveyPage from "./pages/survey.js";
 import PersonsPage from "./pages/persons.js";
 import spaService from "./services/spa.js";
 
 // Declare and init
 let navbar = new NavBar();
 let homePage = new HomePage();
+let surveyPage = new SurveyPage();
 let personsPage = new PersonsPage();
 
 // init services
 spaService.init();
 
 window.pageChange = () => spaService.pageChange();
+
+window.question1 = () => surveyPage.question1();
+
+window.question2 = () => surveyPage.question2();
