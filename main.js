0da40c6101ddce21643fb33a78af977362517404
Mike Francois Hoffmann <mikefrancois1005@gmail.com>
2020-04-20 12:23:35 +0200

    tipspage

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ import NavBar from "./components/navbar.js";
 import HomePage from "./pages/home.js";
 import PersonsPage from "./pages/persons.js";
 import spaService from "./services/spa.js";
+import TipsPage from "./pages/tips.js";
 
 // Declare and init
 let navbar = new NavBar();
 let homePage = new HomePage();
 let personsPage = new PersonsPage();
+let TipsPage = new TipsPage();
 
 // init services
 spaService.init();
