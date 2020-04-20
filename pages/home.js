9ca5af42a040fcec8372bdc22ce2c43cc8fd6fc2
eaajome <48531953+eaajome@users.noreply.github.com>
2020-04-20 11:55:52 +0200

    Added tabbar to left

--- pages/home.js
+++ pages/home.js
@@ -4,11 +4,8 @@ export default class HomePage {
   }
 
   template() {
-    document.getElementById('content').innerHTML += /*html*/ `
+    document.getElementById("content").innerHTML += /*html*/ `
       <section id="home" class="page">
-        <header class="topbar">
-          <h2>Home</h2>
-        </header>
         <h3>SPA Template Modules Pages</h3>
         <p>My Single Page Web App Template</p>
       </section>
