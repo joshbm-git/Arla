5caecdad6b3df65b3a45a6f239371dee2582339a
Mike Francois Hoffmann <mikefrancois1005@gmail.com>
2020-04-20 12:27:40 +0200

    update

--- pages/tips.js
+++ pages/tips.js
@@ -1,5 +1,16 @@
 export default class TipsPage {
 
+    constructor() {
+        this.template();
+    }
 
+    template() {
+        document.getElementById("content").innerHTML += /*html*/ `
+          <section id="home" class="page">
+            <h3>SPA Template Modules Pages</h3>
+            <p>My Single Page Web App Template</p>
+          </section>
+        `;
+    }
 
 }
