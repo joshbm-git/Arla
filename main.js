12f255212453c326ca77f9a25d8baf7e621b25c6
joshbm-git <joshbm.dev@gmail.com>
2020-04-27 11:39:45 +0200

    added icons to survey

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 import LoginPage from "./pages/login.js";
 import HomePage from "./pages/home.js";
 import ChartPage from "./pages/chart.js";
+import SurveyPage from "./pages/survey.js";
 import TipsPage from "./pages/tips.js";
 
 // import your services
@@ -12,6 +13,7 @@ import authService from "./services/auth.js";
 let loginPage = new LoginPage();
 let homePage = new HomePage();
 let tipsPage = new TipsPage();
+let surveyPage = new SurveyPage();
 let chartPage = new ChartPage();
 
 // init services
@@ -31,3 +33,13 @@ window.updateUser = () => profilePage.updateUser();
 window.previewImage = (file, previewId) =>
   profilePage.previewImage(file, previewId);
 window.addDataset = () => chartAddPage.addDataset();
+
+window.pageChange = () => spaService.pageChange();
+window.question1 = () => surveyPage.question1();
+window.question2 = () => surveyPage.question2();
+window.question3 = () => surveyPage.question3();
+window.question4 = () => surveyPage.question4();
+window.question5 = () => surveyPage.question5();
+window.question6 = () => surveyPage.question6();
+window.question7 = () => surveyPage.question7();
+window.progress = () => surveyPage.progress();
---
+++ pages/survey.js
@@ -0,0 +1,273 @@
+export default class SurveyPage {
+  constructor() {
+    this.template();
+    this.numOfQuestions = 6;
+    this.currentQuestion = 1;
+    this.width = 0;
+    this.grow = 100 / this.numOfQuestions;
+  }
+
+  template() {
+    document.getElementById("content").innerHTML += /*html*/ `
+      <section id="survey" class="page">
+       <div class="survey-wrapper">
+      <h2>Questionnaire</h2>
+      
+
+
+      <div class="survey-inner-wrapper">
+      
+      <h3>Please answer these questions for the sustainability of your farm.</h3>
+      
+      <button class="nextBtn" onclick="question1();progress();">Next</button>
+      
+      </div>
+      
+      </div> 
+
+    </section>
+    `;
+  }
+
+  progress() {
+    if (this.currentQuestion <= this.numOfQuestions) {
+      this.currentQuestion++;
+      this.width += this.grow;
+    } else {
+      this.currentQuestion = this.numOfQuestions;
+    }
+    console.log(this.width);
+
+    let bar = document.querySelector(".progressBar");
+
+    bar.style.background = `
+      linear-gradient(to right, var(--green) ${this.width}%, var(--light-green) 0% 100%)
+      `;
+  }
+
+  question1() {
+    document.querySelector(".survey-wrapper").innerHTML = /*html*/ `
+    <h2>Questionnaire</h2>
+      
+    <div class="progressBar">
+    <figure>
+    <img src="../images/cowicon.png">
+    </figure>
+
+    <figure>
+    <img src="../images/tractoricon.png">
+    </figure>
+
+    <figure>
+    <img src="../images/farmicon.png">
+    </figure>
+
+    <figure>
+    <img src="../images/rainicon.png">
+    </figure>
+
+    <figure>
+    <img src="../images/energyicon.png">
+    </figure>
+
+    <figure>
+    <img src="../images/plantsicon.png">
+    </figure>
+    </div> 
+
+
+    <div class="survey-inner-wrapper">
+    
+    <div class="question-type-wrapper">
+    <p class="question-type"></p>
+    <p class="question-number">${this.currentQuestion} / ${this.numOfQuestions}</p>
+    </div>
+
+    <form name="textForm">
+    What is the food consumption in kg of dry matter for each cow per year?
+    <div class="top-form-wrapper">
+
+    <input type="number" placeholder="Kg" name="fname" required>
+    </div>
+    
+    <div class="btn-wrapper">
+    <button onclick="" class="btn btn-back">Back</button>
+    <button type="button" onclick="question2();progress();" class="btn btn-next">Next</button>
+    
+    </div>
+    </form>
+
+  </div>
+                `;
+  }
+
+  question2() {
+    document.querySelector(".survey-inner-wrapper").innerHTML = /*html*/ `
+    
+
+    
+    <div class="question-type-wrapper">
+    <p class="question-type"></p>
+    <p class="question-number">${this.currentQuestion} / ${this.numOfQuestions}</p>
+    </div>
+
+    <form name="textForm">
+    What is the milk production in kg for each year per cow?
+    <div class="top-form-wrapper">
+
+    <input type="number" placeholder="Kg" name="fname" required>
+    </div>
+    
+    <div class="btn-wrapper">
+    <button onclick="" class="btn btn-back">Back</button>
+    <button type="button" onclick="question3();progress();" class="btn btn-next">Next</button>
+    
+    </div>
+    </form>
+
+
+
+                `;
+  }
+
+  question3() {
+    document.querySelector(".survey-inner-wrapper").innerHTML = /*html*/ `
+    
+
+    <div class="question-type-wrapper">
+    <p class="question-type"></p>
+    <p class="question-number">${this.currentQuestion} / ${this.numOfQuestions}</p>
+    </div>
+
+    <form name="textForm">
+    How many cows do you have? 
+    <div class="top-form-wrapper">
+
+    <input type="number" placeholder="No of cows" name="fname" required>
+    </div>
+    
+    <div class="btn-wrapper">
+    <button onclick="" class="btn btn-back">Back</button>
+    <button type="button" onclick="question4();progress();" class="btn btn-next">Next</button>
+    
+    </div>
+    </form>
+
+
+
+                `;
+  }
+
+  question4() {
+    document.querySelector(".survey-inner-wrapper").innerHTML = /*html*/ `
+    
+    
+    <div class="question-type-wrapper">
+    <p class="question-type"></p>
+    <p class="question-number">${this.currentQuestion} / ${this.numOfQuestions}</p>
+    </div>
+
+    <form name="textForm">
+    How much diesel in kg does your farm comsume per day? 
+    <div class="top-form-wrapper">
+
+    <input type="number" placeholder="Kg" name="fname" required>
+    </div>
+    
+    <div class="btn-wrapper">
+    <button onclick="" class="btn btn-back">Back</button>
+    <button type="button" onclick="question5();progress();" class="btn btn-next">Next</button>
+    
+    </div>
+    </form>
+
+
+
+                `;
+  }
+
+  question5() {
+    document.querySelector(".survey-inner-wrapper").innerHTML = /*html*/ `
+    
+    
+    <div class="question-type-wrapper">
+    <p class="question-type"></p>
+    <p class="question-number">${this.currentQuestion} / ${this.numOfQuestions}</p>
+    </div>
+
+    <form name="textForm">
+    How much electricity in kWh does your farm comsume on a daily basis? 
+    <div class="top-form-wrapper">
+
+    <input type="number" placeholder="kWh" name="fname" required>
+    </div>
+    
+    <div class="btn-wrapper">
+    <button onclick="" class="btn btn-back">Back</button>
+    <button type="button" onclick="question6();progress();" class="btn btn-next">Next</button>
+    
+    </div>
+    </form>
+
+  
+
+                `;
+  }
+  question6() {
+    document.querySelector(".survey-inner-wrapper").innerHTML = /*html*/ `
+    
+    
+    <div class="question-type-wrapper">
+    <p class="question-type"></p>
+    <p class="question-number">${this.currentQuestion} / ${this.numOfQuestions}</p>
+    </div>
+
+    <form name="textForm">
+    How self-sufficient are you in feed?
+    <div class="top-form-wrapper">
+
+    <input type="number" placeholder="% of dry matter" name="fname" required>
+    </div>
+    
+    <div class="btn-wrapper">
+    <button onclick="" class="btn btn-back">Back</button>
+    <button type="button" onclick="question7();progress();" class="btn btn-next">Next</button>
+    
+    </div>
+    </form>
+
+
+
+                `;
+  }
+
+  question7() {
+    document.querySelector(".survey-inner-wrapper").innerHTML = /*html*/ `
+    
+  
+    <div class="question-type-wrapper">
+    <p class="question-type"></p>
+    </div>
+
+    <form name="textForm">
+    <h3>Thank you for your parcitipation in making the world sustainable!</h3>
+    <div class="top-form-wrapper">
+
+    
+    </div>
+    
+    <div class="btn-wrapper">
+    
+    
+    </div>
+    </form>
+
+
+
+
+                `;
+  }
+}
+
+{
+}
